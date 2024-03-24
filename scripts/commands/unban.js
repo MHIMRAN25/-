6e8e08fee5cf9260a4c1cfca541f0b8543895d5d
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-03-24 10:17:09 +0600

    Update unban.js

--- scripts/commands/unban.js
+++ scripts/commands/unban.js
@@ -1,131 +1,187 @@
-
 module.exports.config = {
-    name: "unban",
-    version: "1.0.0",
-    Permssion: 0,
-    credits: "Imran", 
-    prefix: true, 
-   description: "Gỡ ban nhóm và người dùng trong 1 nốt nhạc",
-    Category: "Admin",
-    usages: "",
-    cooldowns: 5,
-    denpendencies: {}
-}, module.exports.run = async ({
-    event: a,
-    api: e,
-    Users: n,
-    Threads: t,
-    args: s
-}) => {
-    var {
-        threadID: d,
-        messageID: o,
-        senderID: l
-    } = a;
-    const {
-        commands: r
-    } = global.client;
-    if ("ManhG" != r.get("unban".toLowerCase()).config.credits) return e.sendMessage("Sai credit!", a.threadID, a.messageID);
-    const g = global.data.threadData.get(parseInt(a.threadID)) || {},
-        i = g.hasOwnProperty("PREFIX") ? g.PREFIX : global.config.PREFIX;
-    switch (s[0]) {
-        case "admin":
-        case "ad": {
-            const a = global.config.ADMINBOT;
-            for (var c of a) {
-                const a = (await n.getData(c)).data || {};
-                a.banned = 0, a.reason = null, a.dateAdded = null, await n.setData(c, {
-                    data: a
-                }), global.data.userBanned.delete(c, 1)
-            }
-            e.sendMessage("Đã gỡ ban cho toàn bộ admin bot!", d, o);
-            break
-        }
-        case "allbox":
-        case "allthread": {
-            const a = global.data.threadBanned.keys();
-            for (const e of a) {
-                const a = (await t.getData(e)).data || {};
-                a.banned = 0, a.reason = null, a.dateAdded = null, await t.setData(e, {
-                    data: a
-                }), global.data.userBanned.delete(e, 1)
-            }
-            e.sendMessage("Đã gỡ ban cho toàn nhóm trên server", d, o);
-            break
-        }
-        case "box":
-        case "thread":
-            var b = a.threadID;
-            (h = (await t.getData(b)).data || {}).banned = 0, h.reason = null, h.dateAdded = null, await t.setData(b, {
-                data: h
-            }), global.data.userBanned.delete(b, 1), e.sendMessage("Đã gỡ ban cho nhóm này!", d, o);
-            break;
-        case "allmember":
-        case "alluser": {
-            const a = global.data.userBanned.keys();
-            for (const e of a) {
-                const a = (await n.getData(e)).data || {};
-                a.banned = 0, a.reason = null, a.dateAdded = null, await n.setData(e, {
-                    data: a
-                }), global.data.userBanned.delete(e, 1)
-            }
-            e.sendMessage("Đã gỡ ban cho toàn bộ người dùng trên server", d, o);
-            break
-        }
-        case "qtvall":
-        case "Qtvall":
-        case "allqtv":
-            var h = [];
-            h = await t.getAll();
-            for (let a = 0; a < h.length; a++) {
-                const e = h[a].threadInfo.adminIDs;
-                for (let a = 0; a < e.length; a++) {
-                    const t = e[a].id,
-                        s = (await n.getData(t)).data || {};
-                    s.banned = 0, s.reason = null, s.dateAdded = null, await n.setData(t, {
-                        data: s
-                    }), global.data.userBanned.delete(t, 1)
-                }
-            }
-            e.sendMessage("Đã gỡ ban cho toàn bộ QTV Box trên sever!", d, o);
-            break;
-        case "qtv":
-        case "Qtv":
-            var m = (await t.getData(a.threadID)).threadInfo.adminIDs;
-            for (let a = 0; a < m.length; a++) {
-                const e = m[a].id,
-                    t = (await n.getData(e)).data || {};
-                t.banned = 0, t.reason = null, t.dateAdded = null, await n.setData(e, {
-                    data: t
-                }), global.data.userBanned.delete(e, 1)
-            }
-            e.sendMessage("Đã gỡ ban cho toàn bộ QTV Box này!", d, o);
-            break;
-        case "member":
-        case "mb":
-        case "user":
-            if (!s[1]) {
-                var u = a.participantIDs;
-                for (let a = 0; a < u.length; a++) {
-                    const e = u[a],
-                        t = (await n.getData(e)).data || {};
-                    t.banned = 0, t.reason = null, t.dateAdded = null, await n.setData(e, {
-                        data: t
-                    }), global.data.userBanned.delete(e, 1)
-                }
-                return e.sendMessage("Đã gỡ ban cho toàn bộ thành viên trong nhóm này!", d, o)
-            }
-            if (-1 !== s.join().indexOf("@")) {
-                var D = Object.keys(a.mentions),
-                    f = (await n.getData(D)).userID,
-                    v = (await n.getData(D)).name;
-                const t = (await n.getData(f)).data || {};
-                return t.banned = 0, t.reason = null, t.dateAdded = null, await n.setData(f, {
-                    data: t
-                }), global.data.userBanned.delete(f, 1), e.sendMessage(`${v} đã được gỡ ban!`, d, o)
-            }
-            break;
-        default:
-            e.sendMessage(`Bạn có thể dùng:\n\n${i}${this.config.name} admin => gỡ ban cho toàn bộ admin bot\n\n${i}${this.config.name} allbox => gỡ ban cho toàn bộ nhóm trên sever\n\n${i}${this.config.name} box => gỡ ban cho nhóm hiện tại [1 nhóm ]\n\n${i}${this.config.name} alluser => gỡ ban cho toàn bộ người dùng trên sever\n\n${i}${this.config.name} allqtv => gỡ ban cho toàn bộ QTV Box trên sever\n\n${i}${this.config.name} qtv => gỡ ban cho toàn bộ QTV Box [1 box ]\n\n${i}${this.config.name} member => gỡ ban cho toàn bộ thành viên trong nhóm [1 nhóm ]\n\n${i}${this.config.name} member @[tag] => gỡ ban cho người được tag`, d, o)
-    }
+  name: "unban",
+  version: "1.0.0",
+   Permssion: 2,
+  credits: "ManhG",//Mod by H.Thanh
+  prefix: true,
+  description: "Remove groups and users in 1 note",
+  Category: "Admin",
+  usages: "unban",
+  cooldowns: 5,
+  denpendencies: {}
 };
+
+module.exports.run = async ({ event, api, Users, Threads, args }) => {
+  var { threadID, messageID, senderID } = event;
+  
+  const { commands } = global.client;
+  const command = commands.get(("unban").toLowerCase());
+  const credit = command.config.credits;
+  var mangG = "ManhG";
+  if(credit != mangG) return api.sendMessage(`Sai credit!`, event.threadID, event.messageID);
+  
+  const threadSetting = global.data.threadData.get(parseInt(event.threadID)) || {};
+  const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
+
+  switch (args[0]) {
+    case 'admin':
+    case 'ad':
+      {
+        const listAdmin = global.config.ADMINBOT;
+        for (var idad of listAdmin) {
+          const data = (await Users.getData(idad)).data || {};
+          data.banned = 0;
+          data.reason = null;
+          data.dateAdded = null;
+          await Users.setData(idad, { data });
+          global.data.userBanned.delete(idad, 1);
+        }
+        api.sendMessage("𝗠𝗢𝗗𝗘 - Banned for all Admin Bot", threadID, messageID)
+        break;
+      }
+
+    case 'ndh':
+      {
+        const listNDH = global.config.NDH;
+        for (var idNDH of listNDH) {
+          const data = (await Users.getData(idNDH)).data || {};
+          data.banned = 0;
+          data.reason = null;
+          data.dateAdded = null;
+          await Users.setData(idNDH, { data });
+          global.data.userBanned.delete(idNDH, 1);
+        }
+        api.sendMessage("𝗠𝗢𝗗𝗘 - Banned for all Supporters", threadID, messageID)
+        break;
+      }
+
+
+    case 'allbox':
+    case 'allthread':
+      {
+        const threadBanned = global.data.threadBanned.keys();
+        for (const singleThread of threadBanned) {
+          const data = (await Threads.getData(singleThread)).data || {};
+          data.banned = 0;
+          data.reason = null;
+          data.dateAdded = null;
+          await Threads.setData(singleThread, { data });
+          global.data.userBanned.delete(singleThread, 1);
+        }
+        api.sendMessage("𝗠𝗢𝗗𝗘 - Banned for the entire group on the server", threadID, messageID)
+        break;
+      }
+
+    case 'box':
+    case 'thread':
+      {
+        var idbox = event.threadID;
+        var data = (await Threads.getData(idbox)).data || {};
+        data.banned = 0;
+        data.reason = null;
+        data.dateAdded = null;
+        await Threads.setData(idbox, { data });
+        global.data.userBanned.delete(idbox, 1);
+        api.sendMessage("𝗠𝗢𝗗𝗘 - The ban has been removed for this group", threadID, messageID)
+        break;
+      }
+
+    case 'allmember':
+    case 'alluser':
+      {
+        const userBanned = global.data.userBanned.keys();
+        for (const singleUser of userBanned) {
+          const data = (await Users.getData(singleUser)).data || {};
+          data.banned = 0;
+          data.reason = null;
+          data.dateAdded = null;
+          await Users.setData(singleUser, { data });
+          global.data.userBanned.delete(singleUser, 1);
+        }
+        api.sendMessage("𝗠𝗢𝗗𝗘 - Banned for all users on the server", threadID, messageID)
+        break;
+      }
+
+    case 'qtvall':
+    case 'Qtvall':
+    case 'allqtv':
+      {
+        var data = [];
+        data = await Threads.getAll();
+
+        for (let i = 0; i < data.length; i++) {
+          const idAdmins = (data[i].threadInfo).adminIDs;
+          for (let i = 0; i < idAdmins.length; i++) {
+            const idad = idAdmins[i].id;
+
+            const data = (await Users.getData(idad)).data || {};
+            data.banned = 0;
+            data.reason = null;
+            data.dateAdded = null;
+            await Users.setData(idad, { data });
+            global.data.userBanned.delete(idad, 1);
+          }
+        }
+        api.sendMessage('𝗠𝗢𝗗𝗘- Banned for all Administrators on the server', threadID, messageID);
+        break;
+      }
+
+    case 'qtv':
+    case 'Qtv':
+      {
+        //var threadInfo = await api.getThreadInfo(event.threadID);
+        var threadInfo = (await Threads.getData(event.threadID)).threadInfo;
+        var listQTV = threadInfo.adminIDs;
+        for (let i = 0; i < listQTV.length; i++) {
+          const idQtv = listQTV[i].id;
+          const data = (await Users.getData(idQtv)).data || {};
+          data.banned = 0;
+          data.reason = null;
+          data.dateAdded = null;
+          await Users.setData(idQtv, { data });
+          global.data.userBanned.delete(idQtv, 1);
+        }
+        api.sendMessage("𝗠𝗢𝗗𝗘 - Banned for all Admins of this group", threadID, messageID)
+        break;
+      }
+
+    case 'member':
+    case 'mb':
+    case 'user':
+      {
+        if (!args[1]) {
+         // var threadInfo = await api.getThreadInfo(event.threadID);
+          //var threadInfo = (await Threads.getData(event.threadID)).threadInfo;
+          var listMember = event.participantIDs;
+          for (let i = 0; i < listMember.length; i++) {
+            const idMember = listMember[i];
+            const data = (await Users.getData(idMember)).data || {};
+            data.banned = 0;
+            data.reason = null;
+            data.dateAdded = null;
+            await Users.setData(idMember, { data });
+            global.data.userBanned.delete(idMember, 1);
+          }
+          return api.sendMessage("𝗠𝗢𝗗𝗘 - Banned for all members of this group", threadID, messageID);
+        }
+        if (args.join().indexOf('@') !== -1) {
+          var mentions = Object.keys(event.mentions)
+          var userID = (await Users.getData(mentions)).userID;
+          var nameUser = (await Users.getData(mentions)).name;
+          const data = (await Users.getData(userID)).data || {};
+          data.banned = 0;
+          data.reason = null;
+          data.dateAdded = null;
+          await Users.setData(userID, { data });
+          global.data.userBanned.delete(userID, 1);
+          return api.sendMessage(`𝗠𝗢𝗗𝗘 - User ${nameUser} ban has been removed`, threadID, messageID)
+        }
+        break;
+      }
+
+    default:
+      api.sendMessage(`「    𝗨𝗡𝗕𝗔𝗡    𝗖𝗢𝗡𝗙𝗜𝗚  」\n◆━━━━━━━━━━━◆\n\n𝗠𝗢𝗗𝗘 - unban admin => Remove ban for all Admin Bot\n𝗠𝗢𝗗𝗘 - unban ndh => Unban all Supporters\n𝗠𝗢𝗗𝗘 - unban allbox => Unban the entire group on the server\n𝗠𝗢𝗗𝗘 - unban box => Unban the current group ( 1 group \n𝗠𝗢𝗗𝗘 - unban alluser => Unban all users on the server\n𝗠𝗢𝗗𝗘 - unban allqtv => Remove ban for all boards Via server server administrator\n𝗠𝗢𝗗𝗘 - unban qtv => Remove ban for all admins ( 1 group )\n𝗠𝗢𝗗𝗘 - unban member
+> Type ban for all team members ( 1 group )\n𝗠𝗢𝗗𝗘 - unban member tag => Remove the ban for the person with the tag`, threadID, messageID);
+      break;
+  }
+                        }
